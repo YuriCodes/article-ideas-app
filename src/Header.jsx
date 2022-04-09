e6596b00626e8e350bda2ee5f6aa03e331b359a9
yuri <yuricodesbot@gmail.com>
2022-04-09 03:49:05 -0400

    header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@
 function Header(){
     return(
         <div className="header">
-            <h1>Technical Writing Ideas</h1>
+            <h1>Technical Writing Prompts</h1>
             <p> The hardest part of writing is coming up with a concept idea, not anymore! <br />
             Enter your preferred language in the search bar bellow and find fitting writing prompts according to what you're looking.</p>
         </div>
